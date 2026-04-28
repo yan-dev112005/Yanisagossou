18b3c3a36afb5512d00988142989f9571080723a
Yanis <yanisagossou18@gmail.com>
2026-04-28 10:42:24 +0100

    Mise à jour des modifications

--- forms/contact.php
+++ forms/contact.php
@@ -8,7 +8,7 @@
     die( 'Unable to load the "PHP Email Form" Library!');
   }
 
-  $contact = new PHP_Email_Form;
+  $contact = new $PHP_Email_Form;
   $contact->ajax = true;
   
   $contact->to = $receiving_email_address;
@@ -16,15 +16,6 @@
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
